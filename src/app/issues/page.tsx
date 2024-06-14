4b4b50342af071597f5e34a12c7f1e4a1b1609b3
aamigo <aamigodeveloper@gmail.com>
2024-06-14 14:18:04 +0330

    Refactor: Extract Issues table

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,41 +1,21 @@
 import FilterStatus from "@/components/FilterStatus"
+import IssueList, { IssueQuery, columnNames } from "@/components/IssueList"
 import PaginationIssue from "@/components/PaginationIssue"
-import StatusBadge from "@/components/StatusBadge"
 import { Button } from "@/components/ui/button"
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
+
 import prisma from "@/lib/db"
-import { Issue, Status } from "@prisma/client"
-import { ArrowUp } from "lucide-react"
+import { Status } from "@prisma/client"
 import { Metadata } from "next"
 import Link from "next/link"
 
-
 export const metadata: Metadata = {
-  title: 'Issue Tracker - Issue List',
-  description: "View all project issues"
+  title: "Issue Tracker - Issue List",
+  description: "View all project issues",
 }
 
 interface IssuesPageProps {
-  searchParams: {
-    status: Status
-    orderBy: keyof Issue
-    page: string
-  }
+  searchParams: IssueQuery
 }
-
-const columns: { label: string; value: keyof Issue }[] = [
-  { label: "Issue", value: "title" },
-  { label: "Status", value: "status" },
-  { label: "CreatedAt", value: "createdAt" },
-]
 
 export default async function IssuesPage({ searchParams }: IssuesPageProps) {
   const statuses = Object.values(Status)
@@ -46,9 +26,7 @@ export default async function IssuesPage({ searchParams }: IssuesPageProps) {
 
   const where = { status }
 
-  const orderBy = columns
-    .map((column) => column.value)
-    .includes(searchParams.orderBy)
+  const orderBy = columnNames.includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: "asc" }
     : undefined
 
@@ -72,53 +50,7 @@ export default async function IssuesPage({ searchParams }: IssuesPageProps) {
           <Link href={"/issues/new"}>New Issue</Link>
         </Button>
       </div>
-      <Table>
-        <TableCaption>
-          A List of Your {status ? `${status}` : ""} Issue&apos;s
-        </TableCaption>
-        <TableHeader>
-          <TableRow>
-            {columns.map((column) => (
-              <TableHead
-                key={column.value}
-                className={
-                  column.label === "CreatedAt"
-                    ? "hidden sm:flex items-center"
-                    : undefined
-                }
-              >
-                <Link
-                  href={{
-                    query: { ...searchParams, orderBy: column.value },
-                  }}
-                >
-                  {column.label}
-                </Link>
-                {column.value === searchParams.orderBy && (
-                  <ArrowUp size={15} className="inline" />
-                )}
-              </TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {issues.map((issue) => (
-            <TableRow key={issue.id}>
-              <TableCell>
-                <Link href={`/issues/${issue.id}`} className="hover:underline">
-                  {issue.title}
-                </Link>
-              </TableCell>
-              <TableCell>
-                <StatusBadge status={issue.status} />
-              </TableCell>
-              <TableCell className="hidden sm:block">
-                {issue.createdAt.toISOString()}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <IssueList issues={issues} searchParams={searchParams} />
       <PaginationIssue
         currentPage={page}
         totalIssues={issueCount}
---
+++ src/components/IssueList.tsx
@@ -0,0 +1,87 @@
+import StatusBadge from "@/components/StatusBadge"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Issue, Status } from "@prisma/client"
+import { ArrowUp } from "lucide-react"
+import Link from "next/link"
+
+export interface IssueQuery {
+  status: Status
+  orderBy: keyof Issue
+  page: string
+}
+
+interface IssueListProps {
+  searchParams: IssueQuery
+  issues: Issue[]
+}
+
+export default function IssueList({
+  issues,
+  searchParams: { orderBy, status, page },
+}: IssueListProps) {
+  return (
+    <Table>
+      <TableCaption>
+        A List of Your {status ? `${status}` : ""} Issue&apos;s
+      </TableCaption>
+      <TableHeader>
+        <TableRow>
+          {columns.map((column) => (
+            <TableHead
+              key={column.value}
+              className={
+                column.label === "CreatedAt"
+                  ? "hidden sm:flex items-center"
+                  : undefined
+              }
+            >
+              <Link
+                href={{
+                  query: { status, page, orderBy: column.value },
+                }}
+              >
+                {column.label}
+              </Link>
+              {column.value === orderBy && (
+                <ArrowUp size={15} className="inline" />
+              )}
+            </TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {issues.map((issue) => (
+          <TableRow key={issue.id}>
+            <TableCell>
+              <Link href={`/issues/${issue.id}`} className="hover:underline">
+                {issue.title}
+              </Link>
+            </TableCell>
+            <TableCell>
+              <StatusBadge status={issue.status} />
+            </TableCell>
+            <TableCell className="hidden sm:block">
+              {issue.createdAt.toISOString()}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
+
+const columns: { label: string; value: keyof Issue }[] = [
+  { label: "Issue", value: "title" },
+  { label: "Status", value: "status" },
+  { label: "CreatedAt", value: "createdAt" },
+]
+
+export const columnNames = columns.map((column) => column.value)
