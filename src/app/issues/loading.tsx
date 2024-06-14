74e74ad898bc6440e25340320514d20a2578e1e2
aamigo <aamigodeveloper@gmail.com>
2024-06-14 14:24:28 +0330

    Add loading skeleton to issues list page

---
+++ src/app/issues/loading.tsx
@@ -0,0 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function loading() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-3">
+        <div className="flex items-center justify-between">
+            <Skeleton className="h-[40px] w-[180px]" />
+            <Skeleton className="h-[40px] w-[100px]" />
+        </div>
+        <Skeleton className="h-[480px] w-[970px]" />
+        <Skeleton className="h-[40px] w-[400px]" />
+    </div>
+  )
+}
