78294444af2451f0c953786102a4d5c3d051ef3d
aamigo <aamigodeveloper@gmail.com>
2024-06-21 11:22:39 +0330

    utility function to format createdAt value

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { parseISO, format } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function dateFormat(dateStr: Date | string) {
+  const date = typeof dateStr === "string" ? parseISO(dateStr) : dateStr
+
+  return format(date, "MMMM dd, yyyy")
 }
